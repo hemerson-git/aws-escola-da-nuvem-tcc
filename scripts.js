61414cb7e6cadcff407fa4aefcd1a30cab55cb09
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2023-09-10 19:20:00 -0300

    add curriculums

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const prevButton = document.getElementById("prev-button");
+  const nextButton = document.getElementById("next-button");
+  const buttonContainer = document.querySelector("#button-container");
+
   tns({
     container: ".my-slider",
     autoplay: true,
@@ -6,5 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
     autoplayHoverPause: true,
     autoplayButton: false,
     autoplayButtonOutput: false,
+    arrowKeys: true,
+    controlsContainer: "#button-container",
   });
+
+  feather.replace();
 });
